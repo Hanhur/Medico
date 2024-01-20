ea003113c2aa96bc0852e0e6a85d2aeae428711a
Vladimir Hanhur <aqgz@email.cz>
2024-01-20 18:03:14 +0100

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
