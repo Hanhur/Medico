3a78e8dbb2f68c15c7a60270e97b46f0b061f0b5
Vladimír Hanhur <aqgz@email.cz>
2024-04-28 17:30:26 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        630: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        820: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        980: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1100: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
     },
 });
